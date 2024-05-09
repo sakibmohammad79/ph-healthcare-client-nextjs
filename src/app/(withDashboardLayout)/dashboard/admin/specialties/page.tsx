db14a8957b2b02a706867969c940375aeb708500
sakibmohammad79 <sakibmohammad7679@gmail.com>
2024-05-09 17:36:57 +0600

    added specialites table

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -3,6 +3,20 @@ import { Box, Button, Stack, TextField } from "@mui/material";
 import { useState } from "react";
 import SpecialityModal from "./components/SpecialistModal";
 import { useGetAllSpecialiesQuery } from "@/redux/api/specialtiesApi";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Image from "next/image";
+
+const columns: GridColDef[] = [
+  { field: "title", headerName: "Title", width: 150 },
+  {
+    field: "icon",
+    headerName: "icon",
+    width: 150,
+    // renderCell: ({ row }) => {
+    //   <Image src={row?.icon} alt="specialityImage" />;
+    // },
+  },
+];
 
 const SpecialityPage = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
@@ -18,9 +32,13 @@ const SpecialityPage = () => {
         ></SpecialityModal>
         <TextField size="small" placeholder="Search Specialist"></TextField>
       </Stack>
-      <Box>
-        <h1>{data?.length}</h1>
-      </Box>
+      {!isLoading ? (
+        <Box>
+          <DataGrid rows={data} columns={columns} hideFooter />
+        </Box>
+      ) : (
+        <h2>isLoading....</h2>
+      )}
     </Box>
   );
 };
