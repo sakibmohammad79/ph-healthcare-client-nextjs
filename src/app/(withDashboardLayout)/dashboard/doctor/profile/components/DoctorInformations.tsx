4b21fb7310e3bda56d5fe422672f6a4961aa5239
sakibmohammad79 <sakibmohammad7679@gmail.com>
2024-05-18 15:52:54 +0600

    showing doctor profile data

---
+++ src/app/(withDashboardLayout)/dashboard/doctor/profile/components/DoctorInformations.tsx
@@ -0,0 +1,121 @@
+import { dateFormatter } from "@/utils/dateFormatter";
+import { Box, Grid, Stack, Typography } from "@mui/material";
+import { styled } from "@mui/system";
+import Image from "next/image";
+
+const StyledInformationBox = styled(Box)(({ theme }) => ({
+  background: "#f4f7fe",
+  borderRadius: theme.spacing(1),
+  padding: "8px 16px",
+  width: "45%",
+
+  "& p": {
+    fontWeight: 600,
+  },
+}));
+
+const DoctorInformations = ({ data }: any) => {
+  const formattedTime = dateFormatter(data?.createdAt);
+  return (
+    <>
+      <Grid item xs={12} sm={12} md={4}>
+        <Box
+          sx={{
+            height: 400,
+            width: "100%",
+            overflow: "hidden",
+            borderRadius: 1,
+            mt: 1,
+          }}
+        >
+          <Image
+            src={data?.profilePhoto}
+            alt="Doctor Image"
+            height={400}
+            width={450}
+          ></Image>
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={12} md={8}>
+        <Typography color="primary" component="h5" variant="h5" mb={1}>
+          Personal Information
+        </Typography>
+        <Stack direction={{ sm: "column", md: "row" }} gap={2} flexWrap="wrap">
+          <StyledInformationBox>
+            <Typography variant="caption" color="secondary">
+              Role
+            </Typography>
+            <Typography>{data?.role}</Typography>
+          </StyledInformationBox>
+          <StyledInformationBox>
+            <Typography variant="caption" color="secondary">
+              Name
+            </Typography>
+            <Typography>{data?.name}</Typography>
+          </StyledInformationBox>
+          <StyledInformationBox>
+            <Typography variant="caption" color="secondary">
+              Email
+            </Typography>
+            <Typography>{data?.email}</Typography>
+          </StyledInformationBox>
+          <StyledInformationBox>
+            <Typography variant="caption" color="secondary">
+              Gender
+            </Typography>
+            <Typography>{data?.gender}</Typography>
+          </StyledInformationBox>
+        </Stack>
+        <Typography color="primary" component="h5" variant="h5" mt={4} mb={1}>
+          Professional Information
+        </Typography>
+        <Stack direction={{ sm: "column", md: "row" }} gap={2} flexWrap="wrap">
+          <StyledInformationBox>
+            <Typography variant="caption" color="secondary">
+              Appointment Fee
+            </Typography>
+            <Typography>{data?.appointmentFee}</Typography>
+          </StyledInformationBox>
+          <StyledInformationBox>
+            <Typography variant="caption" color="secondary">
+              Qualification
+            </Typography>
+            <Typography>{data?.qualification}</Typography>
+          </StyledInformationBox>
+          <StyledInformationBox>
+            <Typography variant="caption" color="secondary">
+              Current Working Place
+            </Typography>
+            <Typography>{data?.currentWorkingPlace}</Typography>
+          </StyledInformationBox>
+          <StyledInformationBox>
+            <Typography variant="caption" color="secondary">
+              Average Rating
+            </Typography>
+            <Typography>{data?.averageRating}</Typography>
+          </StyledInformationBox>
+          <StyledInformationBox>
+            <Typography variant="caption" color="secondary">
+              Status
+            </Typography>
+            <Typography>{data?.status}</Typography>
+          </StyledInformationBox>
+          <StyledInformationBox>
+            <Typography variant="caption" color="secondary">
+              Experience
+            </Typography>
+            <Typography>{data?.experience}</Typography>
+          </StyledInformationBox>
+          <StyledInformationBox>
+            <Typography variant="caption" color="secondary">
+              Joined
+            </Typography>
+            <Typography>{formattedTime}</Typography>
+          </StyledInformationBox>
+        </Stack>
+      </Grid>
+    </>
+  );
+};
+
+export default DoctorInformations;
--- src/app/(withDashboardLayout)/dashboard/doctor/profile/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/doctor/profile/page.tsx
@@ -1,81 +1,23 @@
 "use client";
 
 import { useMyProfileQuery } from "@/redux/api/myProfileApi";
+import { dateFormatter } from "@/utils/dateFormatter";
+
 import { Box, Grid, Stack, Typography, styled } from "@mui/material";
 import Image from "next/image";
-
-const StyledInformationBox = styled(Box)(({ theme }) => ({
-  background: "#f4f7fe",
-  borderRadius: theme.spacing(1),
-  padding: "8px 16px",
-  width: "45%",
-
-  "& p": {
-    fontWeight: 600,
-  },
-}));
+import DoctorInformations from "./components/DoctorInformations";
 
 const DoctorProfile = () => {
   const { data, isLoading } = useMyProfileQuery({});
   console.log(data);
+
   if (isLoading) {
     return <h2>isLoading....</h2>;
   }
   return (
     <Box>
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={12} md={4}>
-          <Box
-            sx={{
-              height: 300,
-              width: "100%",
-              overflow: "hidden",
-              borderRadius: 1,
-            }}
-          >
-            <Image
-              src={data?.profilePhoto}
-              alt="Doctor Image"
-              height={300}
-              width={400}
-            ></Image>
-          </Box>
-        </Grid>
-        <Grid item xs={12} sm={12} md={8}>
-          <Typography color="primary" component="h5" variant="h5">
-            Doctor Information
-          </Typography>
-          <Stack
-            direction={{ sm: "column", md: "row" }}
-            gap={2}
-            flexWrap="wrap"
-          >
-            <StyledInformationBox>
-              <Typography variant="caption" color="secondary">
-                Role
-              </Typography>
-              <Typography>{data?.role}</Typography>
-            </StyledInformationBox>
-            <StyledInformationBox>
-              <Typography variant="caption" color="secondary">
-                Name
-              </Typography>
-              <Typography>{data?.name}</Typography>
-            </StyledInformationBox>
-            <StyledInformationBox>
-              <Typography variant="caption" color="secondary">
-                Email
-              </Typography>
-              <Typography>{data?.email}</Typography>
-            </StyledInformationBox>
-            <StyledInformationBox>
-              <Typography variant="caption" color="secondary">
-                Gender
-              </Typography>
-              <Typography>{data?.gender}</Typography>
-            </StyledInformationBox>
-          </Stack>
-        </Grid>
+        <DoctorInformations data={data}></DoctorInformations>
       </Grid>
     </Box>
   );
