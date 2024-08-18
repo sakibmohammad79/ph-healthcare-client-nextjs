975bd37f9d794c476e5714132b98004687aaa755
sakib <mohammadsakib7679@gmail.com>
2024-08-19 00:11:17 +0600

    send password reset url to the email

--- src/app/(withCommonLayout)/forgot-password/page.tsx
+++ src/app/(withCommonLayout)/forgot-password/page.tsx
@@ -1,10 +1,18 @@
 "use client";
-import { Box, Button, Container, Grid, Stack, Typography } from "@mui/material";
+import {
+  Alert,
+  Box,
+  Button,
+  Container,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
 import Image from "next/image";
 import assets from "@/assets";
 import { SubmitHandler, FieldValues } from "react-hook-form";
 import { toast } from "sonner";
-import { storeUserInfo } from "@/services/auth.services";
+import CheckIcon from "@mui/icons-material/Check";
 import PHForm from "@/components/Forms/PHForm";
 import PHInput from "@/components/Forms/PHInput";
 import { z } from "zod";
@@ -16,13 +24,16 @@ const validationSchema = z.object({
 });
 
 const ForgotPasswordPage = () => {
-  const [forgotPassword] = useForgotPasswordMutation();
+  const [forgotPassword, { isSuccess }] = useForgotPasswordMutation();
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     try {
       const res = await forgotPassword(data);
-      // console.log(res);
-
-      toast.success("Check your email for set new password!");
+      console.log(res);
+      if (res?.data?.success == true) {
+        toast.success("Check your email for set new password!");
+      } else {
+        throw new Error("Something went wrong, try again!");
+      }
     } catch (err) {
       console.log(err);
     }
@@ -55,25 +66,32 @@ const ForgotPasswordPage = () => {
           </Stack>
 
           <Box>
-            <PHForm
-              onSubmit={onSubmit}
-              resolver={zodResolver(validationSchema)}
-              defaultValues={{ email: "", password: "" }}
-            >
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={12} md={12} lg={12}>
-                  <PHInput
-                    fullWidth={true}
-                    label="Email"
-                    name="email"
-                    type="text"
-                  />
+            {isSuccess && (
+              <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
+                An email with reset password link was send to your email!
+              </Alert>
+            )}
+            {!isSuccess && (
+              <PHForm
+                onSubmit={onSubmit}
+                resolver={zodResolver(validationSchema)}
+                defaultValues={{ email: "", password: "" }}
+              >
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sm={12} md={12} lg={12}>
+                    <PHInput
+                      fullWidth={true}
+                      label="Email"
+                      name="email"
+                      type="text"
+                    />
+                  </Grid>
                 </Grid>
-              </Grid>
-              <Button type="submit" fullWidth sx={{ my: 3 }}>
-                Forgot Password
-              </Button>
-            </PHForm>
+                <Button type="submit" fullWidth sx={{ my: 3 }}>
+                  Forgot Password
+                </Button>
+              </PHForm>
+            )}
           </Box>
         </Box>
       </Stack>
